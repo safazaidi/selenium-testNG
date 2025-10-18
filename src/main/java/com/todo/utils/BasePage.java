f55952cd71967fb82d016b554a2d4abbddeb5d22
safaz <safazeidi123@gmail.com>
2025-10-18 11:12:55 +0100

    selenium-testNG

---
+++ src/main/java/com/todo/utils/BasePage.java
@@ -0,0 +1,38 @@
+package com.todo.utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BasePage {
+	public static WebDriver driver;
+	public static Properties prop;
+	
+	public BasePage() throws IOException{
+		prop = new Properties();
+		FileInputStream fis = new FileInputStream("src/main/java/com/todo/configs/env-config.properties");
+		prop.load(fis);
+		
+	}
+	
+	public static void initialization(String browser) {
+		if(browser.equals("chrome")) {
+			driver = new ChromeDriver();
+		}
+		else if(browser.equals("firefox")) {
+			driver = new FirefoxDriver();
+		}
+		else if (browser.equals("edge")) {
+			driver = new EdgeDriver();
+		}
+		else {
+			System.out.println("browsev does not exist");       
+		}
+	}
+
+}
---
+++ src/main/java/com/todo/utils/Setup.java
@@ -0,0 +1,29 @@
+package com.todo.utils;
+
+import java.io.IOException;
+
+import org.openqa.selenium.bidi.module.Browser;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+
+public class Setup extends BasePage{
+
+	public Setup() throws IOException {
+		super();	
+	}
+	
+	@Parameters({"browser"})
+	@BeforeMethod
+	public void setupTest(String Browser ) {
+		initialization( Browser);
+		driver.get(prop.getProperty("url"));
+		driver.manage().window().maximize();
+	}
+	
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+	}
+
+}
